63af08fd19bffff01a2ab43c51d1e7abf4a1d82a
underthestars-zhy <zhuhaoyu0909@icloud.com>
2025-11-22 15:30:08 -0800

    Register services in GrpcServer.start
    Iterate over provided service implementations and register each service
    with the underlying nice-grpc server using the generated proto lookup
    (passing an empty implementation object for now).
    
    Also adjust import ordering and minor formatting/semicolon fixes.

--- src/runtime/server.ts
+++ src/runtime/server.ts
@@ -1,22 +1,26 @@
+import type { GrpcObject } from "@grpc/grpc-js";
 import { createServer } from "nice-grpc";
-import { type GrpcObject } from "@grpc/grpc-js";
 import type { ServiceImpl } from "../core/service";
 import { loadProtoFromString } from "../utils/proto-loader";
 import { buildProtoString } from "./proto-builder";
 
-export class GrpcServer{
+export class GrpcServer {
     readonly address: string;
     readonly grpcServer = createServer();
     readonly serviceImpls: ServiceImpl<any, "server">[];
     readonly proto: GrpcObject;
-    
+
     constructor(address: string, serviceImpls: ServiceImpl<any, "server">[]) {
         this.address = address;
-        this.serviceImpls = serviceImpls
-        this.proto = loadProtoFromString(buildProtoString(serviceImpls))
+        this.serviceImpls = serviceImpls;
+        this.proto = loadProtoFromString(buildProtoString(serviceImpls));
     }
-    
+
     async start() {
+        for (const serviceImpl of this.serviceImpls) {
+            this.grpcServer.add(this.proto[serviceImpl.serviceClass.serviceName] as any, {});
+        }
+
         await this.grpcServer.listen(this.address);
     }
 }
