54bc8432cfeac4088ed24c784655f63501189eb1
underthestars-zhy <zhuhaoyu0909@icloud.com>
2025-11-24 21:00:53 -0800

    Add unary tests and handlers for object inputs

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -9,6 +9,8 @@ describe("server side test", async () => {
         clientFn1 = client<() => number>();
         clientFn2 = client<(value: number) => number>();
         clientFn3 = client<(value1: number, value2: number) => number>();
+        clientFn4 = client<(value: { sub1: number, sub2: number }) => number>();
+        clientFn5 = client<(value1: { sub1: number, sub2: number }, value2: { sub1: string, sub2: string }) => number>();
         serverFn1 = server<() => number>();
     }
     
@@ -30,7 +32,13 @@ describe("server side test", async () => {
         },
         clientFn3: async (value1: number, value2: number) => {
             return value1 + value2;
-        }
+        },
+        clientFn4: async (value: { sub1: number, sub2: number }) => {
+            return value.sub1 + value.sub2;
+        },
+        clientFn5: async (value1: { sub1: number, sub2: number }, value2: { sub1: string, sub2: string }) => {
+            return value1.sub1 + value2.sub1.length + value1.sub2 + value2.sub2.length;
+        },
     });
     
 
@@ -55,4 +63,17 @@ describe("server side test", async () => {
         expect(await grpcServer.UnaryTestService.clientFn3(2, 3)).toBe(5);
         expect(await grpcServer.UnaryTestService.clientFn3(3, 4)).toBe(7);
     });
+    
+    test("unary with object input", async () => {
+        expect(await grpcServer.UnaryTestService.clientFn4({ sub1: 1, sub2: 2 })).toBe(3);
+        expect(await grpcServer.UnaryTestService.clientFn4({ sub1: 2, sub2: 3 })).toBe(5);
+        expect(await grpcServer.UnaryTestService.clientFn4({ sub1: 3, sub2: 4 })).toBe(7);
+    });
+    
+    test("unary with multiple object inputs", async () => {
+        expect(await grpcServer.UnaryTestService.clientFn5({ sub1: 1, sub2: 2 }, { sub1: "a", sub2: "b" })).toBe(5);
+        expect(await grpcServer.UnaryTestService.clientFn5({ sub1: 2, sub2: 3 }, { sub1: "aa", sub2: "bb" })).toBe(9);
+        expect(await grpcServer.UnaryTestService.clientFn5({ sub1: 3, sub2: 4 }, { sub1: "aaa", sub2: "bbb" })).toBe(13);
+    });
+
 });
