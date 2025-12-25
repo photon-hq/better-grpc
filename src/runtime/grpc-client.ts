e36562ca9b6beade4e63dea19a8f2c35ca3d6e63
underthestars-zhy <zhuhaoyu0909@icloud.com>
2025-12-25 01:19:10 -0800

    Update default gRPC channel options
    Lower max_receive_message_length to 10000 and max_send_message_length to
    5000. Remove grpc.http2.* ping/min_time options and keep only
    keepalive_permit_without_calls to simplify keepalive settings. Update
    default gRPC channel options
    
    Reduce default message size limits to 10000 (receive) and 5000 (send),
    remove grpc.http2.* ping/interval options, and bump package version to
    0.2.5.

--- src/runtime/grpc-client.ts
+++ src/runtime/grpc-client.ts
@@ -3,14 +3,11 @@ import type { ServiceCallable, ServiceImpl, ServiceNameOf } from "../core/servic
 import { GrpcClient } from "./client";
 
 export const DEFAULT_OPTIONS: ChannelOptions = {
-    "grpc.max_receive_message_length": 10 * 1024 * 1024,
-    "grpc.max_send_message_length": 10 * 1024 * 1024,
+    "grpc.max_receive_message_length": 10000,
+    "grpc.max_send_message_length": 5000,
     "grpc.keepalive_time_ms": 30000,
     "grpc.keepalive_timeout_ms": 10000,
-    "grpc.keepalive_permit_without_calls": 1,
-    "grpc.http2.max_pings_without_data": 0,
-    "grpc.http2.min_time_between_pings_ms": 10000,
-    "grpc.http2.min_ping_interval_without_data_ms": 10000,
+    "grpc.keepalive_permit_without_calls": 1
 };
 
 export async function createGrpcClient<T extends ServiceImpl<any, "client">[]>(
