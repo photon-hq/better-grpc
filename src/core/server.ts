4638cb086bd7a5e46bb657b022e300b46bf0cbce
underthestars-zhy <zhuhaoyu0909@icloud.com>
2025-12-25 23:53:49 -0800

    Make ServerCallable context-aware
    Return callables that provide a withMeta(meta) helper when a signature
    includes a zod metadata context; import ExtractContext and add
    CallableChain to represent the conditional return shape

--- src/core/server.ts
+++ src/core/server.ts
@@ -1,5 +1,5 @@
 import type z from "zod";
-import type { AnyBaseSignature, BaseSignature, ExtractFn, ExtractImplFn, ValidReturnType } from "./base";
+import type { AnyBaseSignature, BaseSignature, ExtractContext, ExtractFn, ExtractImplFn, ValidReturnType } from "./base";
 import type { AnyContext, Context } from "./context";
 
 type ServerSignature<fn extends (...args: any[]) => any, C extends AnyContext | undefined> = BaseSignature<
@@ -11,19 +11,28 @@ type ServerSignature<fn extends (...args: any[]) => any, C extends AnyContext | 
         ? <Meta extends z.ZodObject<any>>(context: { metadata: Meta }) => ServerSignature<fn, Context<Meta>>
         : {});
 
-    
 export function server<fn extends (...args: any[]) => ValidReturnType<fn>>(): ServerSignature<fn, undefined> {
     return null as any;
 }
 
 export type ServerImpls<T> = {
-    [K in keyof T as T[K] extends BaseSignature<"server", any, any>
-        ? K
-        : never]: T[K] extends AnyBaseSignature ? ExtractImplFn<T[K]> : never;
+    [K in keyof T as T[K] extends BaseSignature<"server", any, any> ? K : never]: T[K] extends AnyBaseSignature
+        ? ExtractImplFn<T[K]>
+        : never;
 };
 
 export type ServerCallable<T> = {
     [K in keyof T as T[K] extends BaseSignature<"server", any, any> | BaseSignature<"bidi", any, any>
         ? K
-        : never]: T[K] extends AnyBaseSignature ? ExtractFn<T[K]> : never;
-}
+        : never]: T[K] extends AnyBaseSignature
+        ? (...args: Parameters<ExtractFn<T[K]>>) => CallableChain<ExtractFn<T[K]>, ExtractContext<T[K]>>
+        : never;
+};
+
+type CallableChain<fn extends (...args: any[]) => any, C extends AnyContext | undefined> = C extends Context<infer Meta>
+    ? Meta extends z.ZodObject<any>
+        ? {
+              withMeta(meta: z.infer<Meta>): ReturnType<fn>;
+          }
+        : ReturnType<fn>
+    : ReturnType<fn>;
